6b1709df2f8cf4eb4df977cf88a4a768de47756a
Esenov-Kubanychbek <kubaesenov031005@gmail.com>
2021-02-08 19:53:20 +0000

    made konopki interactive in js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ let btn2 = document.querySelector('#example2')
 let btn3 = document.querySelector('#example3')
 let btn4 = document.querySelector('#example4')
 let btn5 = document.querySelector('#example5')
+
+btn4.addEventListener('click', function(){
+    let number = parseInt(h1.innerText);
+    number++;
+    h1.innerText = number;
+})
+btn5.addEventListener('click', function(){
+    let number = parseInt(h1.innerText);
+    number--;
+    h1.innerText = number;
+})
